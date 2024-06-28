a960081dfe30ba518981aac0466b1fd061c57cae
Jose <jose.sndov@gmail.com>
2024-06-28 12:38:37 -0500

    Updated colors

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const first = 5 + 5;
+const second = 50 - 10;
+const third = 15 * 2 + 9;
+
 alert(
   "You have received this message because you have been chosen to open an important vault. Here is the secret combination " +
-    "10 - 40 - 39"
+    first +
+    "-" +
+    second +
+    "-" +
+    third
 );
